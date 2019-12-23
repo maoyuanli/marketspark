76a6396c9222455ab996304c2836be5ffafb061a
maoyuanli <maoyuan@gmail.com>
2019-12-23 16:54:18 -0500

    javaRDD

--- src/main/java/tweets/TweetSparker.java
+++ src/main/java/tweets/TweetSparker.java
@@ -4,6 +4,8 @@ import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.*;
@@ -14,6 +16,7 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -23,22 +26,25 @@ public class TweetSparker {
             .getOrCreate();
 
     StructField[] tweetSchemaFields = new StructField[]{
-            new StructField("created_at", DataTypes.StringType, true, Metadata.empty()),
+            new StructField("tweets", DataTypes.StringType, true, Metadata.empty()),
             new StructField("user", DataTypes.StringType, true, Metadata.empty()),
             new StructField("text", DataTypes.StringType, true, Metadata.empty())
     };
 
-    StructType tweetSchema = new StructType(tweetSchemaFields);
+    StructType tweetSchema = DataTypes.createStructType(tweetSchemaFields);
 
     TweetFetcher tweetFetcher = new TweetFetcher(new ArrayList<String>(Arrays.asList("realdonaldTrump")));
     String trumpTweetsStr = tweetFetcher.tweetsList();
+
 
     public void sparkup(){
         Gson gson = new Gson();
         JsonObject trumpTweetsJson = gson.fromJson(trumpTweetsStr, JsonObject.class);
         JsonArray tweets= (JsonArray) trumpTweetsJson.get("tweets");
         List<String> tweetsList = gson.fromJson(tweets.toString(), List.class);
-        System.out.println(tweetsList.toString());
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkSession.sparkContext());
+        JavaRDD<String> javaRDD = javaSparkContext.parallelize(tweetsList);
+
     }
 
 }
