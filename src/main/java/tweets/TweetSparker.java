d26be55f6747aca2f72624a88e88e24cd651826b
maoyuanli <maoyuan@gmail.com>
2019-12-23 16:23:17 -0500

    tweets to List<String>

--- src/main/java/tweets/TweetSparker.java
+++ src/main/java/tweets/TweetSparker.java
@@ -1,5 +1,6 @@
 package tweets;
 
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -10,6 +11,7 @@ import org.apache.spark.sql.types.*;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +30,15 @@ public class TweetSparker {
 
     StructType tweetSchema = new StructType(tweetSchemaFields);
 
-    TweetFetcher tweetFetcher = new TweetFetcher(new ArrayList<String>( Arrays.asList("realdonaldTrump") ));
-    String trumpTweetsStr =  tweetFetcher.tweetsList();
+    TweetFetcher tweetFetcher = new TweetFetcher(new ArrayList<String>(Arrays.asList("realdonaldTrump")));
+    String trumpTweetsStr = tweetFetcher.tweetsList();
+
+    public void sparkup(){
+        Gson gson = new Gson();
+        JsonObject trumpTweetsJson = gson.fromJson(trumpTweetsStr, JsonObject.class);
+        JsonArray tweets= (JsonArray) trumpTweetsJson.get("tweets");
+        List<String> tweetsList = gson.fromJson(tweets.toString(), List.class);
+        System.out.println(tweetsList.toString());
+    }
 
 }
